5a35f764e1a320570182d67f761313276578edc7
Xing Guo <higuoxing@gmail.com>
2022-06-02 00:09:41 +0800

    Initial commit for GNU stow support.

--- .emacs.d/scripts/file-tree.el
+++
@@ -1,9 +0,0 @@
-;; This file contains configuration for file tree browser.
-
-(use-package neotree
-  :ensure t)
-(global-set-key (kbd "C-c f") 'neotree-toggle)
-
-;; Display icons
-(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
-(setq neo-smart-open t)
